51777afde197473cd75a8abd06c87fd78ca1f3f3
Oleksii Kyryk <kirikoleksiy112@gmail.com>
2025-08-02 12:52:07 +0300

    complete

--- src/main.js
+++ src/main.js
@@ -1,3 +1,43 @@
-import axios from 'axios';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+import getImagesByQuery from '../js/pixabay-api';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from '../js/render-functions';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const formElem = document.querySelector('.form');
+
+formElem.addEventListener('submit', e => {
+  e.preventDefault();
+  const query = formElem.elements['search-text'].value;
+  handleSearch(query);
+});
+
+function handleSearch(query) {
+  clearGallery();
+  showLoader();
+  getImagesByQuery(query)
+    .then(images => {
+      if (images.length === 0) {
+        iziToast.error({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
+        });
+        return;
+      }
+      createGallery(images);
+    })
+    .catch(() => {
+      iziToast.error({
+        message: 'Error loading images',
+        position: 'topRight',
+      });
+    })
+    .finally(() => {
+      hideLoader();
+    });
+}
